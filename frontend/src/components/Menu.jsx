ca55659af15046c157923fe1cabb563697d39569
feki_mohamed <medfeki33@gmail.com>
2024-12-02 09:30:24 +0100

    corriger l erreur dans menu.jsx

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
+import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Badge, { BadgeProps } from '@mui/material/Badge';
+import Badge from '@mui/material/Badge';
 import { styled } from '@mui/material/styles';
 import IconButton from '@mui/material/IconButton';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { Link } from 'react-router-dom';
-const StyledBadge = styled(Badge)<BadgeProps>(({ theme }) => ({
+
+const StyledBadge = styled(Badge)(({ theme }) => ({
   '& .MuiBadge-badge': {
     right: -3,
     top: 13,
@@ -21,41 +21,44 @@ const StyledBadge = styled(Badge)<BadgeProps>(({ theme }) => ({
 
 const Menu = () => {
   return (
-
     <div>
       <Navbar expand="lg" className="bg-body-tertiary">
-      <Container fluid>
-        <Navbar.Brand href="#">Ecommerce</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link as={Link} to="/categories">Catégories</Nav.Link>
-            <Nav.Link as={Link} to="/scategories">Sous catégories</Nav.Link>
-            <Nav.Link as={Link} to="/articles">Articles</Nav.Link>
-            <Nav.Link as={Link} to="/client">Clients</Nav.Link>
-            <Nav.Link as={Link} to="/cart"> <i class="fa-solid fa-shop"></i></Nav.Link>
-           
-            
-          </Nav>
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">Search</Button>
-          </Form>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+        <Container fluid>
+          <Navbar.Brand href="#">Ecommerce</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: '100px' }}
+              navbarScroll
+            >
+              <Nav.Link href="#action1">Home</Nav.Link>
+              <Nav.Link as={Link} to="/categories">Catégories</Nav.Link>
+              <Nav.Link as={Link} to="/scategories">Sous catégories</Nav.Link>
+              <Nav.Link as={Link} to="/articles">Articles</Nav.Link>
+              <Nav.Link as={Link} to="/client">Clients</Nav.Link>
+              <Nav.Link as={Link} to="/cart">
+                <IconButton aria-label="cart">
+                  <StyledBadge badgeContent={4} color="secondary">
+                    <ShoppingCartIcon />
+                  </StyledBadge>
+                </IconButton>
+              </Nav.Link>
+            </Nav>
+            <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </div>
-  )
-}
+  );
+};
 
-export default Menu
+export default Menu;
