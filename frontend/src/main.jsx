8af05cb49984d4a81bf1386a68906ebad9370083
feki_mohamed <medfeki33@gmail.com>
2024-12-02 09:58:47 +0100

    ajouter style.css

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import "@fortawesome/fontawesome-free/css/all.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "./style.css"
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
