5f5d1cf2748d9534f9c737bb1371976de48fa951
feki_mohamed <medfeki33@gmail.com>
2024-12-16 11:22:16 +0100

    ajouter basic exemples

---
+++ frontend/src/components/articles/Affichearticles.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { useMemo } from 'react';
+import {
+  MaterialReactTable,
+  useMaterialReactTable,
+  
+}from 'material-react-table';
+
+
+const Affichearticles = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Affichearticles
